22d79f1f32b19fee01b767a6af607516a9bd06f7
mgnielsen1 <55125415+mgnielsen1@users.noreply.github.com>
2022-03-17 16:02:11 -0700

    added sqlite db connection

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = '/Users/mmmatt/Documents/software projects/GiftPlannerApp/gift-app/development.db'
+# db = SQLAlchemy(app)
+#
+# @app.route("/")
+# def hello_world():
+#     return render_template('home.html.jinja')
+#
+#
+# # model
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+#
 
-@app.route("/")
-def hello_world():
-    return render_template('home.html.jinja')
+if __name__ == '__main__':
+    app.run()
