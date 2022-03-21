3e38e1515fe4518dc7d9e762137b2d1ed51b497f
mgnielsen1 <55125415+mgnielsen1@users.noreply.github.com>
2022-03-20 19:54:48 -0700

    added server error validation on resources

--- gift-app/database/database.py
+++ gift-app/database/database.py
@@ -15,7 +15,4 @@ Base.query = session.query_property()
 
 
 def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
     Base.metadata.create_all(bind=engine)
--- gift-app/resources.py
+++ gift-app/resources.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask_restful import Resource, reqparse, abort
 from services import UserService
 from database.models import User
@@ -5,8 +6,13 @@ import utils
 
 
 class Home(Resource):
-    def get(self):
-        return 'Home'
+    @staticmethod
+    def get():
+        try:
+            return render_template('home.html.jinja')
+
+        except:
+            abort(500, message='Internal Server Error')
 
 
 class UserResource(Resource):
@@ -15,49 +21,59 @@ class UserResource(Resource):
     def get(id):
         """gets user with id"""
 
-        user = UserService.get_user(id)
+        try:
+            user = UserService.get_user(id)
 
-        if not user:
-            abort(404, message=f'User with id {id} does not exist')
+            if not user:
+                abort(404, message=f'User with id {id} does not exist')
+            return user.serialize(), 200
 
-        return user.serialize(), 200
-
+        except:
+            abort(500, message='Internal Server Error')
 
 class UsersResource(Resource):
     @staticmethod
     def get():
         """gets all users"""
 
-        users = UserService.get_users()
-        if len(users) == 0:
-            return 'no users found', 204
+        try:
+            users = UserService.get_users()
+            if len(users) == 0:
+                return 'no users found', 204
+            return utils.serialize_list(users), 200
 
-        return utils.serialize_list(users), 200
+        except:
+            abort(500, message='Internal Server Error')
 
     @staticmethod
     def post():
         """creates new record with name and return entire user in json"""
 
-        user_post_args = reqparse.RequestParser()
-        user_post_args.add_argument('name', type=str, help='name of the user', required=True)
-        args = user_post_args.parse_args()
+        try:
+            user_post_args = reqparse.RequestParser()
+            user_post_args.add_argument('name', type=str, help='name of the user', required=True)
+            args = user_post_args.parse_args()
 
-        user = User(name=args.name)
-        UserService.save_user(user)
+            user = User(name=args.name)
+            UserService.save_user(user)
+            return user.serialize(), 201
 
-        return user.serialize(), 201
+        except:
+            abort(500, message='Internal Server Error')
 
     @staticmethod
     def delete():
         """deletes user record with id"""
 
-        user_delete_args = reqparse.RequestParser()
-        user_delete_args.add_argument('id', type=int, help='id of the user', required=True)
-        args = user_delete_args.parse_args()
+        try:
+            user_delete_args = reqparse.RequestParser()
+            user_delete_args.add_argument('id', type=int, help='id of the user', required=True)
+            args = user_delete_args.parse_args()
 
-        UserService.delete_user(args.id)
+            UserService.delete_user(args.id)
+            return 'Success', 204
 
-        return 'Success', 204
-
+        except:
+            abort(500, message='Internal Server Error')
 
 
