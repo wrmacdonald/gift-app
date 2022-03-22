763ac74d51f2659e4706040f99c1d35ad59b1968
mgnielsen1 <55125415+mgnielsen1@users.noreply.github.com>
2022-03-21 22:54:38 -0700

    added flask-serialize for improved serialization

--- gift-app/database/models.py
+++ gift-app/database/models.py
@@ -1,14 +1,17 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy_serializer import SerializerMixin
 from database.database import Base
 
 
-class User(Base):
+class User(Base, SerializerMixin):
     __tablename__ = 'user'
     user_id = Column('user_id', Integer, primary_key=True)
     name = Column(String(80), nullable=False)
 
-    def serialize(self) -> dict:
-        return {self.user_id: self.name}
+    def __init__(self, name: str):
+        self.name = name
+
+
 
 
 
--- gift-app/resources.py
+++ gift-app/resources.py
@@ -2,7 +2,6 @@ from flask import render_template
 from flask_restful import Resource, reqparse, abort
 from services import UserService
 from database.models import User
-import utils
 
 
 class Home(Resource):
@@ -32,7 +31,7 @@ class UserResource(Resource):
 
             if not user:
                 abort(404, message=f'User with id {id} does not exist')
-            return user.serialize(), 200
+            return user.to_dict(), 200
 
         except:
             abort(500, message='Internal Server Error')
@@ -57,7 +56,7 @@ class UserResource(Resource):
                 args = user_put_args.parse_args()
 
                 user = UserService.update_user(id, args)
-                return user.serialize(), 200
+                return user.to_dict(), 200
         except:
             abort(500, message='Internal Server Error')
 
@@ -79,7 +78,7 @@ class UsersResource(Resource):
             users = UserService.get_users()
             if len(users) == 0:
                 return 'no users found', 204
-            return utils.serialize_list(users), 200
+            return [user.to_dict() for user in users], 200
 
         except:
             abort(500, message='Internal Server Error')
@@ -102,7 +101,7 @@ class UsersResource(Resource):
 
             user = User(name=args.name)
             UserService.save_user(user)
-            return user.serialize(), 201
+            return user.to_dict(), 201
 
         except:
             abort(500, message='Internal Server Error')
