30934c079e2f00fe02920ff187147e03c508f0cf
mgnielsen1 <55125415+mgnielsen1@users.noreply.github.com>
2022-03-31 22:42:15 -0700

    clean up database.py
    rename DB_HOST2 to DB_HOST

--- gift-app/database/database.py
+++ gift-app/database/database.py
@@ -8,12 +8,10 @@ import logging
 log = logging.getLogger(__name__)
 
 load_dotenv()
-# DB_HOST = os.getenv("DB_HOST")
-# engine = create_engine(DB_HOST)
 
 # connect to mySQL server
-DB_HOST2 = os.getenv("DB_HOST2")
-engine = create_engine(DB_HOST2)
+DB_HOST = os.getenv("DB_HOST")
+engine = create_engine(DB_HOST)
 
 
 session = scoped_session(sessionmaker(autocommit=False,
@@ -21,11 +19,6 @@ session = scoped_session(sessionmaker(autocommit=False,
                                       bind=engine))
 Base = declarative_base()
 Base.query = session.query_property()
-
-# create table
-# print(Base.metadata.tables.values())
-# Base.metadata.drop_all(engine)
-# Base.metadata.create_all(engine)
 
 
 def init_db():
