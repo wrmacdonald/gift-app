a0a80067c952a29f3d6cb5cd1ae6da30af11fdcf
wrmacdonald <wrmacdonald2016@gmail.com>
2022-05-05 16:48:48 -0700

    sending invite email working, need rest to work
    - sends invite email,
    - plan next steps,

--- mail/messages.py
+++ mail/messages.py
@@ -15,12 +15,30 @@ class EmailConfirmationMessage(EmailMessage):
         self.set_content(BodyTemplates.CONFIRMATION_BODY.substitute(url=url))
 
 
+class EmailInviteMessage(EmailMessage):
+
+    def __init__(self, group_id: int, recipient: str):
+        super().__init__()
+
+        url = f'http://localhost:5000/api/groups/{group_id}/invite'
+        self['Subject'] = Subjects.INVITE
+        self['To'] = recipient
+        self['From'] = Config.MAIL_DEFAULT_SENDER
+        self.set_content(BodyTemplates.INVITATION_BODY.substitute(url=url))
+
+
 class Subjects:
     CONFIRMATION = "Confirm your email"
+    INVITE = "You're invited a Gift group"
 
 
 class BodyTemplates:
     CONFIRMATION_BODY = Template(
         'Welcome to the GIFT APP, thanks for signing up!\n\n'
         + 'Please follow this link to confirm your email:$url\n\n'
+        + 'Thanks,\n-The gift app team')
+
+    INVITATION_BODY = Template(
+        "You've been invited to join a group on the GIFT APP!\n\n"
+        + 'Please follow this link to join the group:$url\n\n'
         + 'Thanks,\n-The gift app team')
--- resources/invitation.py
+++ resources/invitation.py
@@ -1,12 +1,53 @@
 import logging
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import jwt_required
+from mail.mail import send_email
+from mail.messages import EmailInviteMessage
+from database.models import Group
 
 log = logging.getLogger(__name__)
 
 
 class InviteMemberResource(Resource):
-    @staticmethod
-    def post():
-        # send invite email to email address
+
+    @jwt_required()
+    def post(self, group_id: int):
+        """
+        send a group invite email to a given email address
+        params:
+        - group id from url
+        - email from body
+        returns:
+        - group does not exist: 404
+        - email not given: 404
+        - 200
+        """
+        try:
+            post_args = reqparse.RequestParser()
+            post_args.add_argument('email', required=True)
+            args = post_args.parse_args()
+
+            if not Group.exists(group_id):
+                return {'message': f'Group with id {group_id} does not exist'}, 404
+
+            if not args.email:
+                return {'message': f'Must provide an email'}, 404
+
+            # get group name? (user who invited them also?) & pass along to email
+
+            # make 2 join links:
+            # Make new account -> go to signup resource, passing (optional) group_id
+            # Already have an account -> add to group resource, passing group_id
+
+            msg = EmailInviteMessage(group_id, args.email)
+            send_email(msg)
+
+            return {'Sent invite email to: ': str(args.email)}, 200
+
+        except Exception as ex:
+            return {'message': 'An internal service error occurred', 'error': str(ex)}, 500
+
+    def get(self):
+        # create a link to share the group_id & let any user join
         # group id
         pass
