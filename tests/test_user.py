7bd31bb7d9a72f0ad06e489f0fbe7ba9198f826f
wrmacdonald <wrmacdonald2016@gmail.com>
2022-05-10 17:22:06 -0700

    test_user in progress
    - some tests working,
    - pytest's parameterize not working yet

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,14 +1,12 @@
+import pytest
 from faker import Faker
 from database.models import User
 from database.database import session
-# from parameterized import parameterized
-# from pytest import fixture
-# # from tests.conftest import db_session
 
 faker = Faker()
 
 
-class Test_User:
+class TestUser:
     def test_create_user(self, test_db):
         """
         create user object
@@ -58,12 +56,97 @@ class Test_User:
 
         assert User.exists(user.id)
 
+    def test_user_not_exist(self, test_db):
+        """
+        check that user_id 5000 does not exist
+        """
+        id = 5000
+
+        assert not User.exists(id)
+
+    def test_get_user(self, test_db):
+        """
+        create user and test that it can be retrieved via get
+        """
+        first_name = faker.first_name()
+        last_name = faker.last_name()
+        email = faker.email()
+        password = faker.password()
+
+        user = User(email=email,
+                    first_name=first_name,
+                    last_name=last_name,
+                    password=password)
+
+        session.add(user)
+        session.commit()
+
+        retrieved_user = User.get(id=user.id)
+        assert retrieved_user.email == email
+        assert retrieved_user.first_name == first_name
+        assert retrieved_user.last_name == last_name
+        assert retrieved_user.password == password
+
+    def test_get_all_users(self, test_db):
+        """
+        delete all rows in User table
+        create num_users and then get them all from db.
+        passes if the correct number of users is returned
+        """
+        session.query(User).delete()
+        session.commit()
+
+        num_users = 10
+        for i in range(num_users):
+            first_name = faker.first_name()
+            last_name = faker.last_name()
+            email = faker.email()
+            password = faker.password()
+
+            user = User(email=email,
+                        first_name=first_name,
+                        last_name=last_name,
+                        password=password)
+
+            session.add(user)
+            session.commit()
+
+        users = User.get_all()
+
+        assert len(users) == num_users
+
+
+@pytest.mark.parametrize([(5,), (1,), (0,)])
+def test_get_all_users_param(self, num_users, test_db):
+    """
+    delete all rows in User table
+    create num_users and then get them all from db.
+    passes if the correct number of users is returned
+    """
+    session.query(User).delete()
+    session.commit()
+
+    for i in range(num_users):
+        first_name = faker.first_name()
+        last_name = faker.last_name()
+        email = faker.email()
+        password = faker.password()
+
+        user = User(email=email,
+                    first_name=first_name,
+                    last_name=last_name,
+                    password=password)
+
+        session.add(user)
+        session.commit()
+
+    users = User.get_all()
+
+    assert len(users) == num_users
+
 
 # update
 
 # get
 
 # delete
-
-
-
